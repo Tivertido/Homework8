c668521225cd6e0764d25d9150afe21bef503c2d
Tema <Tema>
2023-07-09 17:23:54 +0300

    commit_HW8_3

---
+++ HW8_3/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+
+int[,] matrix1 = { { 2, 4 }, { 3, 2 } };
+        int[,] matrix2 = { { 3, 4 }, { 3, 3 } };
+
+        int[,] productMatrix = MultiplyMatrices(matrix1, matrix2);
+
+        Console.WriteLine("Произведение матриц:");
+        PrintMatrix(productMatrix);
+        
+    static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+    {
+        int rows1 = matrix1.GetLength(0);
+        int columns1 = matrix1.GetLength(1);
+        int columns2 = matrix2.GetLength(1);
+
+        int[,] productMatrix = new int[rows1, columns2];
+
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < columns2; j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < columns1; k++)
+                {
+                    sum += matrix1[i, k] * matrix2[k, j];
+                }
+                productMatrix[i, j] = sum;
+            }
+        }
+
+        return productMatrix;
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int columns = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
